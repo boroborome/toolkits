329b47ea1e5a3081f42d69d1065c6940f5b38b34
Yushan Gao <ysgao@thoughtworks.com>
2020-10-17 20:21:27 +0800

    chore: support group maker

---
+++ src/main/java/com/happy3w/toolkits/combination/GroupMaker.java
@@ -0,0 +1,63 @@
+package com.happy3w.toolkits.combination;
+
+import java.util.Arrays;
+import java.util.Spliterators;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public class GroupMaker {
+    public static Stream<int[]> make(int dataSize, int groupSize) {
+        return StreamSupport.stream(new GroupSpliterator(dataSize, groupSize), false);
+    }
+
+
+    private static class GroupSpliterator extends Spliterators.AbstractSpliterator<int[]> {
+        private int dataSize;
+        private int groupSize;
+        private int[] currentValue;
+
+        protected GroupSpliterator(int dataSize, int groupSize) {
+            super(0, 0);
+            this.dataSize = dataSize;
+            this.groupSize = groupSize;
+        }
+
+        @Override
+        public boolean tryAdvance(Consumer<? super int[]> action) {
+            if (currentValue == null) {
+                currentValue = initValue();
+            } else {
+                int[] nextValue = next(currentValue);
+                if (nextValue == null) {
+                    return false;
+                }
+                currentValue = nextValue;
+            }
+
+            action.accept(Arrays.copyOf(currentValue, currentValue.length));
+            return true;
+        }
+
+        private int[] next(int[] values) {
+            for (int digIndex = values.length - 1; digIndex > 0; --digIndex) {
+                int digValue = values[digIndex];
+                if (digValue == 1) {
+                    continue;
+                }
+                values[digIndex - 1]++;
+                values[digIndex] = 1;
+                values[values.length - 1] += (digValue - 2);
+                return values;
+            }
+            return null;
+        }
+
+        private int[] initValue() {
+            int[] newValue = new int[this.groupSize];
+            Arrays.fill(newValue, 1);
+            newValue[groupSize - 1] = dataSize - groupSize + 1;
+            return newValue;
+        }
+    }
+}
---
+++ src/test/java/com/happy3w/toolkits/combination/GroupMakerTest.java
@@ -0,0 +1,41 @@
+package com.happy3w.toolkits.combination;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.*;
+
+public class GroupMakerTest {
+
+    @Test
+    public void should_make_success_when_normal() {
+        String result = GroupMaker.make(5, 3)
+                .map(it -> Arrays.toString(it))
+                .collect(Collectors.joining("\n"));
+        Assert.assertEquals("[1, 1, 3]\n" +
+                "[1, 2, 2]\n" +
+                "[1, 3, 1]\n" +
+                "[2, 1, 2]\n" +
+                "[2, 2, 1]\n" +
+                "[3, 1, 1]", result);
+    }
+
+    @Test
+    public void should_make_success_when_single() {
+        String result = GroupMaker.make(5, 1)
+                .map(it -> Arrays.toString(it))
+                .collect(Collectors.joining("\n"));
+        Assert.assertEquals("[5]", result);
+    }
+
+    @Test
+    public void should_make_success_when_full() {
+        String result = GroupMaker.make(5, 5)
+                .map(it -> Arrays.toString(it))
+                .collect(Collectors.joining("\n"));
+        Assert.assertEquals("[1, 1, 1, 1, 1]", result);
+    }
+}
