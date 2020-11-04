35e104702f1770b085e1c2aa72fa71d252ac837a
Yushan Gao <ysgao@thoughtworks.com>
2020-11-05 00:50:14 +0800

    chore: make Point24 easy

--- src/test/java/com/happy3w/toolkits/point24/Point24Playground.java
+++ src/test/java/com/happy3w/toolkits/point24/Point24Playground.java
@@ -8,7 +8,7 @@ public class Point24Playground {
     @Test
     public void test_operFlowStrategy() {
         Point24Strategy point24Strategy = new OperFlowStrategy();
-        long resultCount = point24Strategy.judge(new int[]{1, 2, 3, 4, 5, 6})
+        long resultCount = point24Strategy.judge(new int[]{3, 4, 6, 8})
                 .peek(exp -> System.out.println(exp))
                 .count();
         System.out.println(MessageFormat.format("Total case count:{0}", point24Strategy.getJudgedExpCount()));
