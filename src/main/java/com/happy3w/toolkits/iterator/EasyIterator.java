e7dbf8d3fe0b19397ce6472c7c8ed6e1f42558d3
Yushan Gao <ysgao@thoughtworks.com>
2020-11-30 19:39:03 +0800

    chore: Support different type when create EasyIterator; Fix bug when create Iterator; add Empty Iterator

--- src/main/java/com/happy3w/toolkits/iterator/EasyIterator.java
+++ src/main/java/com/happy3w/toolkits/iterator/EasyIterator.java
@@ -5,16 +5,31 @@ import java.util.Iterator;
 import java.util.stream.Stream;
 
 public abstract class EasyIterator<T> implements IEasyIterator<T> {
+    public static final IEasyIterator EMPTY_IT = new IEasyIterator() {
+        @Override
+        public boolean hasNext() {
+            return false;
+        }
 
-    public static <T> EasyIterator<T> from(Stream<T> stream) {
-        return new IteratorExtIterator<>(stream.iterator());
+        @Override
+        public Object next() {
+            return null;
+        }
+    };
+
+    public static <T, E extends T> IEasyIterator<T> fromStream(Stream<E> stream) {
+        return stream == null ? emptyIterator() : new IteratorExtIterator<>(stream.iterator());
     }
 
-    public static <T> EasyIterator<T> from(Iterator<T> iterator) {
-        return new IteratorExtIterator<>(iterator);
+    public static <T, E extends T> IEasyIterator<T> fromIterator(Iterator<E> iterator) {
+        return iterator == null ? emptyIterator() : new IteratorExtIterator<>(iterator);
     }
 
-    public static <T> EasyIterator<T> of(T... values) {
+    public static <T, E extends T> IEasyIterator<T> fromIterable(Iterable<E> iterable) {
+        return iterable == null ? emptyIterator() : new IteratorExtIterator<T, E>(iterable.iterator());
+    }
+
+    public static <T> IEasyIterator<T> of(T... values) {
         return new IteratorExtIterator<>(Arrays.asList(values).iterator());
     }
 
@@ -22,4 +37,7 @@ public abstract class EasyIterator<T> implements IEasyIterator<T> {
         return new IntRangeIterator(start, end);
     }
 
+    public static <T> IEasyIterator<T> emptyIterator() {
+        return EMPTY_IT;
+    }
 }
