690b89151276f9794e74bcb06146b0fa70822692
Yushan Gao <ysgao@thoughtworks.com>
2020-12-03 00:06:10 +0800

    refactor: make TypeItemManager extends from AbstractConfigManager

--- src/main/java/com/happy3w/toolkits/manager/TypeItemManager.java
+++ src/main/java/com/happy3w/toolkits/manager/TypeItemManager.java
@@ -1,8 +1,13 @@
 package com.happy3w.toolkits.manager;
 
-public class TypeItemManager<DT, T extends ITypeItem<DT>> extends AbstractTypeProcessorManager<DT, T, TypeItemManager<DT, T>> {
+public class TypeItemManager<DT, T extends ITypeItem<DT>> extends AbstractConfigManager<T, TypeItemManager<DT, T>> {
 
     public void registItem(T item) {
-        registProcess(item.getType(), item);
+        regist(item.getType(), item);
+    }
+
+    public static <DT, T extends ITypeItem<DT>> TypeItemManager<DT, T> inherit() {
+        return new TypeItemManager<DT, T>()
+                .finder((IConfigFinder<T>) InheritFinder.INSTANCE);
     }
 }
