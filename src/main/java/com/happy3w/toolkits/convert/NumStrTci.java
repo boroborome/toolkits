9a68d6a90c44805632addec0ef44578a84bd7004
Yushan Gao <ysgao@thoughtworks.com>
2021-10-10 15:16:00 +0800

    chore: trim 0 when convert num to string

--- src/main/java/com/happy3w/toolkits/convert/NumStrTci.java
+++ src/main/java/com/happy3w/toolkits/convert/NumStrTci.java
@@ -7,6 +7,21 @@ public class NumStrTci extends AbstractTci<Number, String> {
 
     @Override
     public String toTarget(Number source) {
-        return source.toString();
+        String result = source.toString();
+        int index = result.indexOf('.');
+        if (index > 0 && isAllZereBehind(result, index)) {
+            result = result.substring(0, index);
+        }
+        return result;
+    }
+
+    private boolean isAllZereBehind(String result, int index) {
+        for (int end = result.length(), cur = index + 1;
+            cur < end; cur++) {
+            if (result.charAt(cur) != '0') {
+                return false;
+            }
+        }
+        return true;
     }
 }
