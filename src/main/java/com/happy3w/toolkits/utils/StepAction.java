005c242beaef86549aa483f2710ec919d4fd0b41
Yushan Gao <ysgao@thoughtworks.com>
2022-03-18 17:39:40 +0800

    chore: StepAction support param and release 1.0.3

--- src/main/java/com/happy3w/toolkits/utils/StepAction.java
+++ src/main/java/com/happy3w/toolkits/utils/StepAction.java
@@ -2,23 +2,33 @@ package com.happy3w.toolkits.utils;
 
 import lombok.Getter;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 @Getter
-public class StepAction {
+public class StepAction<T> {
     private long count;
     private final long step;
-    private final Consumer<Long> action;
+    private final BiConsumer<Long, T> action;
 
     public StepAction(long step, Consumer<Long> action) {
+        this.step = step;
+        this.action = (s, p) -> action.accept(s);
+    }
+
+    public StepAction(long step, BiConsumer<Long, T> action) {
         this.step = step;
         this.action = action;
     }
 
     public void increase() {
+        increase(null);
+    }
+
+    public void increase(T param) {
         count++;
         if (count % step == 0 && action != null) {
-            action.accept(count);
+            action.accept(count, param);
         }
     }
 }
